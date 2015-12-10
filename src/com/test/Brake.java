50668728d947547e1f4e3b5f759667d3b7d53afb
dave-bt <beattie_dave@hotmail.com>
2015-12-10 16:56:51 +0000

    dwedwd

--- src/com/test/Brake.java
+++ src/com/test/Brake.java
@@ -6,4 +6,10 @@ public class Brake {
 	public Brake() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
 }
